809193802acfa017ea3c1b7db2212fb11195f635
Murray <murray@minty.org>
2009-06-14 15:00:35 +0100

    Simplify the logic in main search controller
    - Set all the stash variables in one go
    - use map { ... } rather than 'push ... for @...'
    - remove / condense some todo comments

--- lib/PSNIC/Controller/Search.pm
+++ lib/PSNIC/Controller/Search.pm
@@ -8,38 +8,29 @@ my $ROWS_PER_PAGE = 100;
 
 # XXX Snippets are the next interesting part.
 # If we searched for a sub name, then we should snippet any pod section that matches
-#
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
 
-    # XXX The default behaviour should be set higher up
-    $c->stash->{template} = 'search.tt';
-
     my $r     = $c->request;
-    my $query = $r->query_params->{q};
-    $c->stash->{pod_page} = 1
-        if $r->query_params->{inline};
-
+    my $query = $r->query_params->{q} || $c->response->redirect('/');
     my %query = tokenize($query);
-    my @conds;
-    push @conds, expand_match_against($_ => $query{$_})
-        for keys %query;
+    my @conds = map { expand_match_against($_ => $query{$_}) } keys %query;
 
-    # XXX Convert to use search
-    # XXX Convert to alternative stash style
-    # http://search.cpan.org/~ribasushi/DBIx-Class-0.08102/lib/DBIx/Class/ResultSet.pm#search_literal
-    $c->stash->{query} = $query;
-    $c->stash->{results} = $c->model('DB::InstalledModule')->search({},{
-        page => 1, # Show lots of results & have no pagination
-        rows => $ROWS_PER_PAGE,
-    })->search_literal(@conds);
+    $c->stash({
+        template => 'search.tt',
+        pod_page => $r->query_params->{inline} ? 1 : 0,
+        query    => $query,
+        results  => scalar $c->model('DB::InstalledModule')->search({},{
+                        page => 1,
+                        rows => $ROWS_PER_PAGE,
+                    })->search_literal(@conds),
+    });
 }
 
 # XXX this wants to contain the ft index expansions
 my %cmds = map { ( "$_:" => 1 ) } qw<mod sub pod code>;
 
-# XXX Wants to handle "phrase matching"
-# XXX and negation
+# XXX Wants to handle "phrase matching" and -negation
 sub tokenize {
     my ($query) = @_;
 
